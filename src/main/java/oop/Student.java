b19f98e65759275579025a9c5eab39ab2440e4a0
upsadin <upsadin@gmail.com>
2022-02-02 00:35:28 +0300

    1.2. Вызов метода объекта [#166085]

---
+++ src/main/java/oop/Student.java
@@ -0,0 +1,19 @@
+package oop;
+
+public class Student {
+    public void song(){
+        System.out.println("I believe I can fly");
+    }
+    public void music() {
+        System.out.println("tra ta ta");
+    }
+    public static void main(String[] args) {
+        Student petya = new Student();
+        petya.music();
+        petya.music();
+        petya.music();
+        petya.song();
+        petya.song();
+        petya.song();
+    }
+}
