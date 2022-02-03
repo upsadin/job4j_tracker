ba9480a7c3a471fb76e9e8488f103acb6b9e0262
upsadin <upsadin@gmail.com>
2022-02-03 21:58:20 +0300

    1.5. Поля объекта [#166083]

---
+++ src/main/java/oop/Cat.java
@@ -0,0 +1,29 @@
+package oop;
+
+public class Cat {
+    private String food;
+    private String name;
+
+    public void eat(String meat) {
+        this.food = meat;
+    }
+
+    public void giveNick(String nick) {
+        this.name = nick;
+    }
+
+    public void show() {
+        System.out.println("Котик " + name + " ел " + food);
+    }
+
+    public static void main(String[] args) {
+        Cat gav = new Cat();
+        Cat black = new Cat();
+        gav.giveNick("Гав");
+        gav.eat("Котлета");
+        black.giveNick("Черный кот");
+        black.eat("Рыба");
+        gav.show();
+        black.show();
+    }
+}
