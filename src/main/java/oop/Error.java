830d7c6b750e24c1337917268dda314f7593bf0a
upsadin <upsadin@gmail.com>
2022-02-06 16:01:17 +0300

    1.7. Конструктор [#173889]

---
+++ src/main/java/oop/Error.java
@@ -0,0 +1,31 @@
+package oop;
+
+public class Error {
+    boolean active;
+    int status;
+    String message;
+
+    public Error () {
+    }
+
+    Error (boolean active, int status, String message) {
+        this.active = active;
+        this.status = status;
+        this.message = message;
+    }
+
+    public void printInfo() {
+        System.out.println("active: " + active);
+        System.out.println("status: " + status);
+        System.out.println("message: " + message);
+    }
+
+    public static void main(String[] args) {
+        Error error1 = new Error();
+        Error error2 = new Error(false, 1, "ошибка");
+        Error error3 = new Error(true, 0, "ошибки нет");
+        error1.printInfo();
+        error2.printInfo();
+        error3.printInfo();
+    }
+}
