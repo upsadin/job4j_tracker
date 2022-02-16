904e43a1e5b41e725e3d56715d06bd5d12d4e0ef
upsadin <upsadin@gmail.com>
2022-02-16 23:08:48 +0300

    2. Перегрузка конструктора [#173890]

--- src/main/java/ru/job4j/tracker/Item.java
+++ src/main/java/ru/job4j/tracker/Item.java
@@ -4,16 +4,16 @@ public class Item {
     private int id;
     private String name;
 
-    public Item() {
+    public Item(int id, String name) {
+        this.id = id;
+        this.name = name;
     }
 
     public Item(String name) {
         this.name = name;
     }
 
-    public Item(int id, String name) {
-        this.id = id;
-        this.name = name;
+    public Item() {
     }
 
     public int getId() {
