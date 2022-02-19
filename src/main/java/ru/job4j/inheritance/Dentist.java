16d8f607ddf43534d71f4a64006e7d5dedc95fb9
upsadin <upsadin@gmail.com>
2022-02-20 00:45:39 +0300

    3. Реализация профессий в коде [#6837]

--- src/main/java/ru/job4j/inheritance/Dentist.java
+++ src/main/java/ru/job4j/inheritance/Dentist.java
@@ -1,4 +1,10 @@
 package ru.job4j.inheritance;
 
 public class Dentist extends Doctor {
+    private boolean newBormashina;
+
+    public Dentist(String name, String surname, String education, String birthday, int experience,boolean newBormashina) {
+        super(name, surname, education, birthday, experience);
+        this.newBormashina = newBormashina;
+    }
 }
