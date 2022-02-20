fad797f6c367da4adf6fce1bb8790eedf8e99862
upsadin <upsadin@gmail.com>
2022-02-20 16:05:53 +0300

    4. Переопределение [#174890]

--- src/main/java/ru/job4j/inheritance/JSONReport.java
+++ src/main/java/ru/job4j/inheritance/JSONReport.java
@@ -2,7 +2,7 @@ package ru.job4j.inheritance;
 
 public class JSONReport extends TextReport {
     public String generate(String name, String body) {
-        return "{" + System.lineSeparator() + "\t\"name\" : \"" + name +"\"," + System.lineSeparator() +
-                "\t\"body\" : \"" + body +"\"" + System.lineSeparator() + "}";
+        return "{" + System.lineSeparator() + "\t\"name\" : \"" + name + "\"," + System.lineSeparator()
+                + "\t\"body\" : \"" + body + "\"" + System.lineSeparator() + "}";
     }
 }
