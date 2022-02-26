72e5e0148dca0b93c2348e803ec89e8fcab32004
upsadin <upsadin@gmail.com>
2022-02-26 16:46:10 +0300

    5. Tracker - хранилище [#396 #269332]

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -15,15 +15,7 @@ public class Tracker {
     }
 
     public Item[] findAll() {
-        Item[] rsl = new Item[size];
-        int count = 0;
-        for (int i = 0; i < size - 1; i++) {
-            if (items[i] != null) {
-                rsl[count++] = items[i];
-            }
-        }
-        rsl = Arrays.copyOf(rsl, count);
-        return rsl;
+        return Arrays.copyOf(items, size);
     }
 
     public Item[] findByName(String key) {
@@ -39,14 +31,24 @@ public class Tracker {
     }
 
     public Item findById(int id) {
-        Item rsl = null;
-        for (int index = 0; index < size; index++) {
-            Item item = items[index];
-            if (item.getId() == id) {
-                rsl = item;
+        int index = indexOf(id);
+        return index != -1? items[index] : null;
+    }
+
+    private int indexOf(int id) {
+        int rsl = -1;
+        for (int i = 0; i < size; i++) {
+            if (items[i].getId() == id) {
+                rsl = i;
                 break;
             }
         }
         return rsl;
     }
+
+/*    public boolean replace(int id, Item item) {
+
+  *//*
+  }
+*/
 }
