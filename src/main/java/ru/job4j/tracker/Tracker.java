4da34b8b8e5c917f1cc1daa04727f6c16be6a048
upsadin <upsadin@gmail.com>
2022-02-27 22:54:11 +0300

    8. Что такое валидация? [#246864]

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -58,12 +58,12 @@ public class Tracker {
 
     public boolean delete(int id) {
         int index = indexOf(id);
-        boolean rsl = index != -1;
-        if (rsl) {
+        boolean check = index != -1;
+        if (check) {
             System.arraycopy(items, index + 1, items, index, size - index - 1);
             items[size - 1] = null;
             size--;
         }
-        return rsl;
+        return check;
     }
 }
