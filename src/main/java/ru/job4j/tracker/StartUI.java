439d82c65bd9c4b3b1173d9fe28bd13d5ff50418
upsadin <upsadin@gmail.com>
2022-03-03 22:12:09 +0300

    2.3. Реализация класса StartUI. Добавление заявки [#500744]

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -8,19 +8,16 @@ public class StartUI {
         boolean run = true;
         while (run) {
             showMenu();
-            System.out.print("Выберите пункт меню: ");
+            System.out.print("Select: ");
             int select = Integer.parseInt(scanner.nextLine());
             if (select == 0) {
-                System.out.println("=== Создание новой заявки ===");
-                System.out.println("Введите имя заявки: ");
+                System.out.println("=== Create a new Item ===");
+                System.out.print("Enter name: ");
                 String name = scanner.nextLine();
                 Item item = new Item(name);
                 tracker.add(item);
-                System.out.println("Добавлена заявка: " + item);
-            }
-            if (select != 6) {
-                System.out.println("Пользователь выбрал: " + select);
-            } else {
+                System.out.println("Добавленная заявка: " + item);
+            } else if (select == 6) {
                 run = false;
             }
         }
