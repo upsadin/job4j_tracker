52705ef10c5ada9c1098570a36002aa9cdd9fc8f
upsadin <upsadin@gmail.com>
2022-07-17 00:41:33 +0300

    0.4. Пользовательские исключения. [#219367]

---
+++ src/main/java/ru/job4j/ex/FindEl.java
@@ -0,0 +1,24 @@
+package ru.job4j.ex;
+
+public class FindEl {
+    public static int indexOf(String[] value, String key) throws ElementNotFoundException {
+        int rsl = -1;
+        for (int i = 0; i < value.length; i++) {
+            if (value[i].equals(key)) {
+                rsl = i;
+            }
+        }
+        if (rsl == -1) {
+            throw new ElementNotFoundException("Element not found");
+        }
+        return rsl;
+    }
+
+    public static void main(String[] args) {
+        try {
+            FindEl.indexOf(new String[]{"q", "w", "e"}, "r");
+        } catch (ElementNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+}
