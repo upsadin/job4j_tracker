c02e8067574fc18b190b8a3511ec7ed5d61613bd
upsadin <upsadin@gmail.com>
2022-08-11 01:18:00 +0300

    1. Singleton [#94606]

--- src/main/java/ru/job4j/SingleTracker.java
+++ src/main/java/ru/job4j/SingleTracker.java
@@ -1,5 +1,8 @@
 package ru.job4j;
 
+import ru.job4j.tracker.Item;
+import ru.job4j.tracker.Tracker;
+
 public class SingleTracker {
     private static SingleTracker instance;
 
@@ -11,5 +14,31 @@ public class SingleTracker {
             instance = new SingleTracker();
         }
         return instance;
+    }
+
+    private Tracker tracker = new Tracker();
+
+    public Item add(Item item) {
+        return tracker.add(item);
+    }
+
+    public Item findByID(int id) {
+        return tracker.findById(id);
+    }
+
+    public Item[] findAll() {
+        return tracker.findAll();
+    }
+
+    public Item[] findByName(String key) {
+        return tracker.findByName(key);
+    }
+
+    public boolean replace(int id, Item item) {
+        return tracker.replace(id, item);
+    }
+
+    public boolean delete(int id) {
+        return tracker.delete(id);
     }
 }
