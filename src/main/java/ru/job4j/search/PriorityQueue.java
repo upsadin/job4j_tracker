d565cbff2defde7dfe27c33f5b425ac5d3cb886a
upsadin <upsadin@gmail.com>
2022-12-08 21:32:07 +0300

    2. Очередь с приоритетом на LinkedList [#41670 #368382]

--- src/main/java/ru/job4j/search/PriorityQueue.java
+++ src/main/java/ru/job4j/search/PriorityQueue.java
@@ -7,11 +7,10 @@ public class PriorityQueue {
     public void put(Task task) {
         int index = 0;
         for (Task element : tasks) {
-            if (task.getPriority() >= element.getPriority()) {
-                index++;
-            } else {
+            if (task.getPriority() < element.getPriority()) {
                 break;
             }
+            index++;
         }
         this.tasks.add(index, task);
     }
