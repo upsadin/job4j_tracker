581f1bb58e4bf3b909ae747d24e044281e2168a3
upsadin <upsadin@gmail.com>
2022-12-11 19:47:43 +0300

    1. Паспорт и жители [#212656 #370056]

--- src/test/java/ru/job4j/collection/PassportOfficeTest.java
+++ src/test/java/ru/job4j/collection/PassportOfficeTest.java
@@ -11,4 +11,15 @@ public class PassportOfficeTest {
         office.add(citizen);
         assertThat(office.get(citizen.getPassport())).isEqualTo(citizen);
     }
+
+    @Test
+    public void whenDuplicatePassport() {
+        Citizen citizen = new Citizen("2f44a", "Petr Arsentev");
+        Citizen citizen2 = new Citizen("2f44a", "Pavel Pavlov");
+        PassportOffice office = new PassportOffice();
+        office.add(citizen);
+        office.add(citizen2);
+        assertThat(office.get(citizen2.getPassport())).isEqualTo(citizen2);
+    }
+
 }
