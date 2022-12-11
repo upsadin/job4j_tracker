a979d5130e2b31726803b65edb365c34aa15f792
upsadin <upsadin@gmail.com>
2022-12-11 14:35:41 +0300

    . Модели данных и HashSet. [#218530]

--- src/main/java/ru/job4j/collection/Account.java
+++ src/main/java/ru/job4j/collection/Account.java
@@ -32,10 +32,13 @@ public class Account {
 
     @Override
     public String toString() {
-        return "Account{" +
-                "passport='" + passport + '\'' +
-                ", username='" + username + '\'' +
-                ", deposit='" + deposit + '\'' +
-                '}';
+        return "Account{"
+                + "passport='"
+                + passport + '\''
+                + ", username='"
+                + username + '\''
+                + ", deposit='"
+                + deposit + '\''
+                + '}';
     }
 }
