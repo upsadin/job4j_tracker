ae89bebb59ed4ad1e1359916e0a38cfe7cfe566b
upsadin <upsadin@gmail.com>
2023-01-28 11:44:36 +0300

    NonUniqueString

--- src/main/java/ru/job4j/map/NonUniqueString.java
+++ src/main/java/ru/job4j/map/NonUniqueString.java
@@ -7,7 +7,7 @@ public class NonUniqueString {
     public static Map<String, Boolean> checkData(String[] strings) {
         Map<String, Integer> map = new HashMap<>();
         for (String string : strings) {
-            int n = map.getOrDefault(string, 1);
+            int n = map.getOrDefault(string, 0);
             map.put(string, n + 1);
         }
         Map<String, Boolean> rsl = new HashMap<>();
