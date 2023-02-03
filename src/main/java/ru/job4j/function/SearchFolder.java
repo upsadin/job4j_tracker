c038413efbd335841faa58bf25619234ab234e7b
upsadin <upsadin@gmail.com>
2023-02-03 20:37:16 +0300

    2.3. Встроенные функциональные интерфейсы. [#249209]

---
+++ src/main/java/ru/job4j/function/SearchFolder.java
@@ -0,0 +1,27 @@
+package ru.job4j.function;
+
+import java.util.List;
+import java.util.ArrayList;
+import java.util.function.Predicate;
+
+public class SearchFolder {
+    public static List<Folder> filterSize(List<Folder> list) {
+        List<Folder> rsl = new ArrayList<>();
+        for (Folder f : list) {
+            if (f.getSize() > 100) {
+                rsl.add(f);
+            }
+        }
+        return rsl;
+    }
+
+    public static List<Folder> filter(List<Folder> list, Predicate<Folder> pred) {
+        List<Folder> rsl = new ArrayList<>();
+        for (Folder f : list) {
+            if (pred.test(f)) {
+                rsl.add(f);
+            }
+        }
+        return rsl;
+    }
+}
