ed066e55df54db45899e21a3af6dcdde6e1ed13d
upsadin <upsadin@gmail.com>
2023-02-14 21:34:00 +0300

    0. Stream API улучшения [#58167]

---
+++ src/main/java/ru/job4j/stream/StudentLevel.java
@@ -0,0 +1,14 @@
+package ru.job4j.stream;
+
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Stream;
+
+public class StudentLevel {
+    public static List<Student> levelOf(List<Student> students, int bound) {
+        return students.stream()
+                .flatMap(Stream::ofNullable)
+                .sorted((left, right) -> right.getScore() - left.getScore())
+                .takeWhile(s -> s.getScore() > bound).toList();
+    }
+}
