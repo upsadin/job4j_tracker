42bb8a01986f819c7e53608ccf927ac4265cb7b4
upsadin <upsadin@gmail.com>
2023-05-14 21:30:33 +0300

    9. Лямбда выражения и Collection Framework [#505026]

--- src/main/java/ru/job4j/stream/FilterNegativeNumbers.java
+++ src/main/java/ru/job4j/stream/FilterNegativeNumbers.java
@@ -9,7 +9,6 @@ public class FilterNegativeNumbers {
     public static void main(String[] args) {
         List<Integer> numbers = Arrays.asList(2, -1, 5, 14, 0, -7, 5);
         List<Integer> positive = numbers.stream().filter(i -> i > 0).collect(Collectors.toList());
-       // positive.remove(1);
         positive.forEach(System.out::println);
     }
 }
